fa2bb71197111476956e7ce9cb90b9a32f7a2312
Mikael Djurfeldt <mikael@djurfeldt.com>
2013-07-04 16:14:11 +0000

    New files in libpyneurosim: pyneurosim.cpp, pyneurosim.h.

---
+++ libpyneurosim/pyneurosim.cpp
@@ -0,0 +1,38 @@
+/*
+ *  pyneurosim.cpp
+ *
+ *  This file is part of libneurosim.
+ *
+ *  Copyright (C) 2013 INCF
+ *
+ *  libneurosim is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libneurosim is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "pyneurosim.h"
+
+namespace PNS {
+
+  ConnectionGenerator*
+  unpackConnectionGenerator (PyObject* pObj)
+  {
+    return 0;
+  }
+
+  void
+  rescanConnectionGenerator ()
+  {
+  }
+
+}
